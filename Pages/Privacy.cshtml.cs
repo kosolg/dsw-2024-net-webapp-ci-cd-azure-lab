4577a82ffc9c93761a43d246aab73f9cdcfdae8b
kosolg <kosior.olga1@gmail.com>
2024-03-23 10:55:27 +0100

    modyfikacje pierwsza wersja

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dsw_2024_net_webapp_ci_cd_azure_lab.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
